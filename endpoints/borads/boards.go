10bd6da5724bd66ea97ddc47fa36434e9670ae0d
sakaitakuya <sakaitakuya@team-lab.com>
2019-12-16 19:00:06 +0900

    モックちょっと修正

--- endpoints/borads/boards.go
+++ endpoints/borads/boards.go
@@ -14,19 +14,19 @@ func List(c echo.Context) error {
 			Model:     gorm.Model{ID: 1, CreatedAt: time.Time{}, UpdatedAt: time.Time{}, DeletedAt: nil},
 			UserId:    1,
 			Title:     "a",
-			DispOrder: 0,
+			DispOrder: 1,
 		},
 		{
 			Model:     gorm.Model{ID: 2, CreatedAt: time.Time{}, UpdatedAt: time.Time{}, DeletedAt: nil},
 			UserId:    1,
 			Title:     "b",
-			DispOrder: 0,
+			DispOrder: 2,
 		},
 		{
 			Model:     gorm.Model{ID: 3, CreatedAt: time.Time{}, UpdatedAt: time.Time{}, DeletedAt: nil},
 			UserId:    1,
 			Title:     "c",
-			DispOrder: 0,
+			DispOrder: 3,
 		},
 	}
 
